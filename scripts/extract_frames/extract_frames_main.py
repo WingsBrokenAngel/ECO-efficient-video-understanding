7c9b92ed8c8bbb563775c157ff475b1465b4393e
神使 <15393667+WingsBrokenAngel@users.noreply.github.com>
2019-03-14 10:37:04 +0800

    Update extract_frames_main.py

--- scripts/extract_frames/extract_frames_main.py
+++ scripts/extract_frames/extract_frames_main.py
@@ -5,6 +5,9 @@
 
 import os
 import subprocess
+import multiprocessing as mp
+import time
+
 
 
 Path_Train = '/home/chenhaoran/kinetics-400-video/train'
@@ -36,32 +39,72 @@ def iterate_vids(vids, out_path):
 
 
 def iterate_dirs(inpath, outpath):
+    num = mp.Value('i', 0)
+    lock = mp.Lock()
+    plist = []
+
     dirs = get_all_dirs(inpath)
-    for idx, d in enumerate(dirs):
+    queue = mp.Queue()
+    for d in dirs:
+        queue.put(d)
+
+    plist.append(mp.Process(target=print_info, args=(num, len(dirs), inpath)))
+
+    for idx in range(16):
+        p = mp.Process(target=single_process, args=(queue, lock, num, idx, outpath))
+        plist.append(p)
+
+    for p in plist:
+        p.start()
+
+    for p in plist:
+        p.join()
+
+
+def print_info(num, total_num, inpath):
+    while num.value != total_num:
+        time.sleep(60)
+        print('\r%s   *** %5.2f%% have been finished.' % 
+          (os.path.basename(inpath), num.value / total_num * 100.), end='')
+
+
+
+def single_process(queue, lock, num, idx, outpath):
+    while True:
+        try:
+            d = queue.get(True, 1)
+        except:
+            print('Process: %d' % idx, 'has finished')
+            break
+
         vids = os.listdir(d)
         full_vids = [os.path.join(d, v) for v in vids]
         out_dir = os.path.join(outpath, os.path.basename(d))
         if not os.path.exists(out_dir):
             os.mkdir(out_dir)
+
         iterate_vids(full_vids, out_dir)
-        print('\r%s   *** %5.2f%% have been finished.' % 
-              (os.path.basename(inpath), (idx + 1.) / len(dirs)), end='')
+        
+        lock.acquire()
+        num.value += 1
+        lock.release()
+
 
 
 def main():
-    if not os.path.exists(Path_Train_Out):
-        os.mkdir(Path_Train_Out)
-    iterate_dirs(Path_Train, Path_Train_Out)
+    # if not os.path.exists(Path_Train_Out):
+    #     os.mkdir(Path_Train_Out)
+    # iterate_dirs(Path_Train, Path_Train_Out)
 
     if not os.path.exists(Path_Val_Out):
         os.mkdir(Path_Val_Out)
 
-    iterate_dirs(Path_Val_Out, Path_Val_Out)
+    iterate_dirs(Path_Val, Path_Val_Out)
 
-    if not os.path.exists(Path_Test_Out):
-        os.mkdir(Path_Test_Out)
+    # if not os.path.exists(Path_Test_Out):
+    #     os.mkdir(Path_Test_Out)
 
-    iterate_dirs(Path_Test, Path_Test_Out)
+    # iterate_dirs(Path_Test, Path_Test_Out)
 
 
 if __name__ == "__main__":
