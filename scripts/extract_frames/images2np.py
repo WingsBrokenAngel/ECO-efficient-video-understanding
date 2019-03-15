86838bae56c64a5d3123d1e899cfd55482f45224
神使 <15393667+WingsBrokenAngel@users.noreply.github.com>
2019-03-15 20:39:46 +0800

    Create images2np.py

---
+++ scripts/extract_frames/images2np.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import cv2
+
+
+kinetics_path = '/home/chenhaoran/kinetics-400-video'
+train_path = 'train_frm'
+val_path = 'val_frm'
+
+
+train_frm_full_path = os.path.join(kinetics_path, train_path)
+val_frm_full_path = os.path.join(kinetics_path, val_path)
+
+train_classes = os.listdir(train_frm_full_path)
+train_full_path = [os.path.join(train_frm_full_path, c) for c in train_classes]
+
+val_classes = os.listdir(val_frm_full_path)
+val_full_path = [os.path.join(val_frm_full_path, v) for v in val_classes]
+
+for path in train_full_path:
+    img_names = os.listdir(path)
+    img_full_paths = [os.path.join(path, img_name) for img_name in img_names]
+    imgs = []
+    for img_path in img_full_paths:
+        img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        imgs.append(img)
+
+    imgs = np.stack(imgs, axis=0)
+    print(path, imgs.shape, imgs.dtype)
+    np.save(os.path.join(path, 'imgs'), imgs)
+
+
+for path in val_full_path:
+    img_names = os.listdir(path)
+    img_full_paths = [os.path.join(path, img_name) for img_name in img_names]
+    imgs = []
+
+    for img_path in img_full_paths:
+        img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        imgs.append(img)
+
+    imgs = np.stack(imgs, axis=0)
+    print(path, imgs.shape, imgs.dtype)
+    np.save(os.path.join(path, 'imgs'), imgs)
