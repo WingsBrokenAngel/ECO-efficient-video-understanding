b53c044b32162ec6b7a0dbe49b375956f06e889c
神使 <15393667+WingsBrokenAngel@users.noreply.github.com>
2019-03-15 21:25:39 +0800

    Update images2np.py

--- scripts/extract_frames/images2np.py
+++ scripts/extract_frames/images2np.py
@@ -2,6 +2,7 @@
 import numpy as np
 import cv2
 import os
+import glob
 
 
 kinetics_path = '/home/chenhaoran/kinetics-400-video'
@@ -19,16 +20,15 @@ val_classes = os.listdir(val_frm_full_path)
 val_full_path = [os.path.join(val_frm_full_path, v) for v in val_classes]
 
 for path in train_full_path:
-    vid_names = os.listdir(path)
-    vid_full_paths = [os.path.join(path, vid_name) for vid_name in vid_names]
+    vid_full_paths = glob.glob(os.path.join(path, '*'))
     for vid_path in vid_full_paths:
-        img_names = os.listdir(vid_path)
-        if len(img_names) < 12:
+        img_full_paths = glob.glob(os.path.join(vid_path, '*'))
+        if len(img_full_paths) < 12:
             continue
         imgs = []
-        for img_name in img_names:
-            img_full_path = os.path.join(vid_path, img_name)
-            print(img_full_path)
+        for img_full_path in img_full_paths:
+            if img_full_path[-4:] == '.npy':
+                continue
             img = cv2.imread(img_full_path, cv2.IMREAD_COLOR)
             img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
             imgs.append(img)
@@ -39,15 +39,15 @@ for path in train_full_path:
 
 
 for path in val_full_path:
-    vid_names = os.listdir(path)
-    vid_full_paths = [os.path.join(path, vid_name) for vid_name in vid_names]
+    vid_full_paths = glob.glob(os.path.join(path, '*'))
     for vid_path in vid_full_paths:
-        img_names = os.listdir(vid_path)
-        if len(img_names) < 12:
+        img_full_paths = glob.glob(os.path.join(vid_path, '*'))
+        if len(img_full_paths) < 12:
             continue
         imgs = []
-        for img_name in img_names:
-            img_full_path = os.path.join(vid_path, img_name)
+        for img_full_path in img_full_paths:
+            if img_full_path[-4:] == '.npy':
+                continue
             img = cv2.imread(img_full_path, cv2.IMREAD_COLOR)
             img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
             imgs.append(img)
